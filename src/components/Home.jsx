17e589811ebfe5593c9edf8cd8805a67aa1fad5b
Julian <julianrosenthal@yahoo.com>
2021-08-23 12:27:14 -0700

    home renders data from mds

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,14 @@ import Signup from './Signup';
 import Contact from './Contact'
 import { connect } from 'react-redux';
 import QRCode from './QRCode'
+import Logo from './Logo.jsx'
 
 
 
 const Home = (props) => {
-  const [data, setData] = useState('')
-  const [scroll, setScroll] = useState({scrollY: 0, opacity: 0, translate: 0})
+  const [part1, setPart1] = useState('')
+  const [part2, setPart2] = useState('')
+  const [scroll, setScroll] = useState({scrollY: 0, opacity: 20, translate: 0})
 
   const checkLoginStatus = (props) => {
     if (JSON.parse(localStorage.getItem('token'))) {
@@ -20,53 +22,79 @@ const Home = (props) => {
 
   const handleScroll = (e) => {
     let scrollY = window.scrollY
-    if (scrollY < 300 ) setScroll({scrollY, opacity: 100, translate: 0})
-    else if (scrollY < 335) setScroll({scrollY, opacity: 80, translate: 20})
-    else if (scrollY < 370) setScroll({scrollY, opacity: 60, translate: 40})
-    else if (scrollY < 405) setScroll({scrollY, opacity: 40, translate: 60})
-    else if (scrollY < 440) setScroll({scrollY, opacity: 20, translate: 80})
+    if (scrollY < 480 ) setScroll({scrollY, opacity: 100, translate: 0})
+    else if (scrollY < 510) setScroll({scrollY, opacity: 80, translate: 20})
+    else if (scrollY < 540) setScroll({scrollY, opacity: 60, translate: 40})
+    else if (scrollY < 570) setScroll({scrollY, opacity: 40, translate: 60})
+    else if (scrollY < 600) setScroll({scrollY, opacity: 20, translate: 80})
 
   }
 
   useEffect(() => {
+    console.log('here')
     checkLoginStatus(props)
     window.addEventListener('scroll', handleScroll)
-    // imports the md file, move to backend
-    import('../mds/text.md')
+  }, [props], window.removeEventListener('scroll', handleScroll));
+
+  useEffect(() => {
+    console.log('here')
+    import('../mds/home-2.md')
       .then(res => {
         fetch(res.default)
           .then(res => res.text())
-          .then(res => setData(res))
+          .then(res => setPart2(res))
       })
       .catch(err => console.log(err))
-    
+  }, [])
 
-  }, [], window.removeEventListener('scroll', handleScroll));
-
-  const logoStyle = {
-    width: "278px",
-    height: "200px",
-    backgroundImage: "url(/logo.png)",
-    backgroundPosition: "center",
-  }
-
+  useEffect(() => {
+    console.log('here')
+    import('../mds/home-1.md')
+      .then(res => {
+        fetch(res.default)
+          .then(res => res.text())
+          .then(res => setPart1(res))
+      })
+      .catch(err => console.log(err))
+  }, [])
   
+  const renderMid = () => {
+    if (part1 && part2) {
+      return (
+        <>
+          <ReactMarkdown children={part1} />
+          <div style={{textAlign: "center"}}>
+            <QRCode 
+              width="300" 
+              height="300" 
+              opacity={scroll.opacity} 
+              translate={scroll.translate} 
+              fill="#91677D"
+            />
+          </div>
+          <ReactMarkdown children={part2} />
+        </>
+      )
+    } else {
+      return 'loading...'
+    }
+  
+  }
   return (
     <div className="home-page">
 
       <div className="top content">
-        
-        <div className="flex-center">
-          <section style={ logoStyle } />
+
+        <div>
+          {/* keep logo 4:3*/}
+          <Logo height="300" width="400" />
         </div>
-        
-        <QRCode width="400" height="400" opacity={scroll.opacity} translate={scroll.translate} />
+      
       </div>
 
 
       <div className="mid content align-left">
-        {/* Make loading component */}
-        {data ? <ReactMarkdown children={data} /> : 'loading'}
+        {renderMid()}
       </div>
 
       <div className="bottom content">
